4c78ac61bf079153a8752026c94ea9091843d8ca
Jason <740675@aacps.org>
2020-10-09 20:14:08 +0000

    I made the ellipse go off the screen with nothing but variables.

--- index.java
+++ index.java
@@ -1,3 +1,11 @@
+
+int cor1 = 250;
+int radius = 150;
+
+int color = 150;
+
+int str0ke = 0;
+
 void setup() {
 	size(500, 500);
 }
@@ -16,10 +24,17 @@ void draw() {
 
 // Step 4: Make it so when the program runs the ball slides off the screen exiting at the point (500, 500)
 
-  background(255);
-  stroke(0);
+background(color);
 
-
+println(mouseX, mouseY);
   
+cor1 = cor1 + 1;
+
+
+stroke(str0ke);
+ellipse(cor1, cor1, radius, radius)
+  
+
+
 
 }
