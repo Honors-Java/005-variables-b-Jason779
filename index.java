87841a02805284a46ec1dbc3da3b2b60e2da00bd
Jason <740675@aacps.org>
2020-10-18 19:24:58 +0000

    I made the spotty dots and box

--- index.java
+++ index.java
@@ -1,16 +1,31 @@
 
-int cor1 = 250;
-int radius = 150;
+int var1 = random(25, 400);
+int var2 = random(25, 400);
+int var3 = random(25, 400);
+int var4 = random(25, 400);
 
-int color = 150;
-
-int str0ke = 0;
+int r1 = random(1,255);
+int g1 = random(1,255);
+int b1 = random(1,255);
 
 void setup() {
 	size(500, 500);
+  rect(CENTER);
+  ellipseMode(CENTER);
+  rectMode(CENTER);
+  frameRate(30);
+}
+
+void mousePressed(){
+fill(random(0,255), random(0,255), random(0,255));
+rect(random(0,500), random(0,500), random(200,300), random(200,300));
 }
 
 void draw() {
+
+
+println(mouseX, mouseY);
+
 // Step 1: Write code that draws the following screenshots 
 // with hard-coded values. (Feel free to use colors 
 // instead of grayscale.)
@@ -24,17 +39,26 @@ void draw() {
 
 // Step 4: Make it so when the program runs the ball slides off the screen exiting at the point (500, 500)
 
-background(color);
-
-println(mouseX, mouseY);
-  
-cor1 = cor1 + 1;
-
-
-stroke(str0ke);
-ellipse(cor1, cor1, radius, radius)
-  
-
+//These are the ellipses
+fill(random(0,255), random(0,255), random(0,255));
+ellipse(mouseX - random(-30,30), mouseY - random(-30,30), 10, 10)
+fill(r1, g1, b1);
+rect(var1, var2, var3, var4);
 
 
 }
+
+void mousePressed(){
+fill(random(0,255), random(0,255), random(0,255));
+
+background(250);
+var1 = random(0, 500);
+var2 = random(0, 500);
+var3 = random(50, 400);
+var4 = random(50, 400);
+
+r1 = random(1,255);
+g1 = random(1,255);
+b1 = random(1,255);
+}
+
